8aab4f527e0fe65ed6f19f70370aedbdbb068128
Dmitry Shevyakov <stormbringer-s@mail.ru>
2017-02-02 15:20:47 +0400

    added 2 simples

---
+++ CSharpTestConsole/TestConsole/TestConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	double d=6;
+        	Console.WriteLine(Math.Sign(d));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DealEventStateMachine/StateMachine/StateMachine/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+
+namespace StateMachine
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity workflow1 = new DealEventStateMashine();
+            WorkflowInvoker.Invoke(workflow1);
+            Console.ReadKey();
+        }
+    }
+}
