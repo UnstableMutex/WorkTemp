dd200899e54ca6abf84d0934de897154a1b34f64
Dmitry <stormbringer-s@mail.ru>
2017-02-02 21:17:42 +0400

    add project

---
+++ TestUWP/TestUWP.Common/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUWP.Common
+{
+
+    public interface IModule
+    {
+        string RelativeFN { get; set; }
+        string Name { get; set; }
+    }
+    public interface IGroup
+    {
+        string Name { get; }
+        string Path { get; }
+    }
+}
---
+++ TestUWP/TestUWP.SampleModule/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestUWP.Common;
+
+namespace TestUWP.SampleModule
+{
+    [Export(typeof(IModule))]
+    public class SampleModule : IModule
+    {
+        public SampleModule()
+        {
+            Name = "Notepad";
+            RelativeFN = "Notepad.exe";
+        }
+        public string Name { get; set; }
+        public string RelativeFN { get; set; }
+    }
+}
