b3cb24d315fbbd1735f2de51f7b0e362326426fe
Dmitry Shevyakov <stormbringer-s@mail.ru>
2017-02-03 18:25:27 +0400

    added wpf solution

---
+++ TestWPF/TestWPF.Common/IModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWPF.Common
+{
+    public interface IName
+    {
+        string Name { get; }
+    }
+    public interface IModule:IName
+    {
+        string FileName { get; }
+    }
+}
---
+++ TestWPF/TestWPF.Common/IModuleGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWPF.Common
+{
+    interface IModuleGroup:IName
+    {
+        string Directory { get; }
+    }
+}
