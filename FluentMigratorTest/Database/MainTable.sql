3d78f2f2f2308f02edd589f35b4ccabae01f51e9
Dmitry <stormbringer-s@mail.ru>
2017-04-06 20:52:32 +0400

    added temp migrator sample

---
+++ FluentMigratorTest/Database/MainTable.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[MainTable]
+(
+	[ID] INT NOT NULL PRIMARY KEY DEFAULT next value for MainTablePKSeq, 
+    [Name] VARCHAR(50) NOT NULL
+)
---
+++ FluentMigratorTest/Migrations/AddSubTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace Migrations
+{
+    [Migration(20170406)]
+    public class AddSubTable:Migration
+    {
+        public override void Up()
+        {
+            Create.Table("SubTable").
+                WithColumn("ID").AsInt32().Identity().
+                WithColumn("Name").AsString(50).NotNullable().
+                WithColumn("MainTable_ID").AsInt32().ForeignKey("MainTable", "ID");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("SubTable");
+        }
+    }
+}
