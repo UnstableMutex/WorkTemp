02950655d7ada5ae9698a521ad5a37be836f6cb4
Dmitry <stormbringer-s@mail.ru>
2017-04-07 23:45:19 +0400

    add runner console and migration

---
+++ FluentMigratorTest/Console.bat
@@ -0,0 +1,1 @@
+packages\FluentMigrator.1.6.2\tools\migrate --conn "Data Source=.;Database=DataBase;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True" --provider SqlServer2014 --assembly "Migrations\bin\Debug\Migrations.dll" --task migrate --version 20170407
--- FluentMigratorTest/Migrations/AddSubTable.cs
+++ FluentMigratorTest/Migrations/AddSubTable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace Migrations
     {
         public override void Up()
         {
+     
             Create.Table("SubTable").
                 WithColumn("ID").AsInt32().Identity().
                 WithColumn("Name").AsString(50).NotNullable().
@@ -21,6 +23,19 @@ namespace Migrations
         public override void Down()
         {
             Delete.Table("SubTable");
+        }
+    }
+    [Migration(20170407)]
+   public  class AddMainTableData:Migration
+    {
+        public override void Up()
+        {
+            Insert.IntoTable("MainTable").Row(new {Name = "SampleData"});
+        }
+
+        public override void Down()
+        {
+           Delete.FromTable("MainTable").AllRows();
         }
     }
 }
