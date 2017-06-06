18ceedf5afb195aa13ae739e742e6e04345ed870
Dmitry <stormbringer-s@mail.ru>
2017-06-06 18:07:08 +0400

    added xamlreader load sample

---
+++ MoveControls/WpfApplication1/Area.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для Area.xaml
+    /// </summary>
+    public partial class Area : UserControl
+    {
+        public Area()
+        {
+            InitializeComponent();
+        }
+        private void Thumb_OnDragDelta(object sender, DragDeltaEventArgs e)
+        {
+
+            var move = new Vector(e.HorizontalChange, e.VerticalChange);
+            MoveOnCanvas(thumbTarget, move);
+        }
+
+        void MoveOnCanvas(UIElement control, Vector move)
+        {
+            Canvas.SetLeft(control, Canvas.GetLeft(control) + move.X);
+            Canvas.SetTop(control, Canvas.GetTop(control) + move.Y);
+        }
+    }
+}
