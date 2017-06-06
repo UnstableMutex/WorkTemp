193b0aad79af956c6f042fc46c337e7eeb136c5e
Dmitry <stormbringer-s@mail.ru>
2017-06-06 11:47:01 +0400

    added movecontrol tesp proj

--- CSharpTestConsole/TestConsole/TestConsole/Program.cs
+++ CSharpTestConsole/TestConsole/TestConsole/Program.cs
@@ -7,39 +7,15 @@ namespace TestConsole
 {
     class Program
     {
-        private const  int StepCount = 500;
-        private const decimal startPrice = 5;
-
-        private const decimal endPrice = 1.5M;
-
-        private static decimal stepprice;
         static void Main(string[] args)
         {
-            decimal sum;
-            for (int i = 1; i < StepCount; i++)
-            {
-                var res = realY(i);
-               Console.WriteLine(res);
-            }
-            Console.ReadKey();
-
+            var i = 400;
+            var test1 = (test) i;
         }
-
-    
-
-      static  decimal stepPrice(decimal step)
-        {
-            var realX = -.007M * step+5.007M;
-            return realX;
-        }
-
-     static   decimal realY(int step)
-        {
-            var tmp = 860*step/500 ;
-            return stepPrice(tmp);
-        }
-
-             
-      
+    }
+    [Flags]
+    enum test:int
+    {
+        one =1, two =2 , four=4,e=8,s=16,b=32,c=64,d=128,e1=256
     }
 }
---
+++ MoveControls/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Thumb_OnDragDelta(object sender, DragDeltaEventArgs e)
+        {
+
+            var move = new Vector(e.HorizontalChange, e.VerticalChange);
+            MoveOnCanvas(thumbTarget, move);
+        }
+
+        void MoveOnCanvas(UIElement control, Vector move)
+        {
+            Canvas.SetLeft(control, Canvas.GetLeft(control) + move.X);
+            Canvas.SetTop(control, Canvas.GetTop(control) + move.Y);
+        }
+
+    
+    }
+}
