15b3f37b889e2fe74ffa212acfbaa2ec32ca780d
Dmitry <stormbringer-s@mail.ru>
2017-06-16 18:32:09 +0400

    added extproperty sample to TestConsole.csproj

--- CSharpTestConsole/TestConsole/TestConsole/Program.cs
+++ CSharpTestConsole/TestConsole/TestConsole/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using IDbDataRecordExtentionsLib;
 
 namespace TestConsole
 {
@@ -9,10 +11,51 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            var i = 400;
-            var test1 = (test) i;
+            var mapper = new Mapper<User>();
+            User u;
+            using (var conn = new SqlConnection("Server=.;Database=Test;Trusted_Connection=True;"))
+            {
+                string q = "select Name,Value from sys.extended_properties where class=4 and major_id=7";
+                conn.Open();
+                using (var cmd=new SqlCommand(q,conn))
+                {
+                    using (var r=cmd.ExecuteReader())
+                    {
+                        u = mapper.Map(r);
+                    }
+                }
+            }
         }
     }
+
+    class User
+    {
+        public string Surname { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+    }
+
+    class Mapper<T> where T:new()
+    {
+        public T Map(SqlDataReader r)
+        {
+            T item = new T();
+          var props=  item.GetType().GetProperties();
+
+            while (r.Read())
+            {
+             var name=   r.GetString("Name");
+                var p = props.SingleOrDefault(x => x.Name == name);
+                if (p != null)
+                {
+                    p.SetValue(item,r.GetString("Value"),null);
+                }
+
+            }
+            return item;
+        }
+    }
+
     [Flags]
     enum test:int
     {
