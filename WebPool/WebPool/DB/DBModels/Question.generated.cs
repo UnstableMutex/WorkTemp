c9a329c79c04d5ad5bd567c36593ea63ae872c10
Dmitry <stormbringer-s@mail.ru>
2017-09-30 15:12:38 +0400

    added db entities

--- WebPool/WebPool/DB/DBModels/Question.generated.cs
+++ WebPool/WebPool/DB/DBModels/Question.generated.cs
@@ -10,8 +10,15 @@ namespace WebPool.DB.DBModels
     public class Question
     {
 		public int ID  { get; set; }
-		public short Pool_ID  { get; set; }
+		public short PoolID  { get; set; }
 		public string Question1  { get; set; }
-		public sbyte QuestionType_ID  { get; set; }
+		public sbyte QuestionTypeID  { get; set; }
+		public DateTime tmp1  { get; set; }
+		public DateTime tmp2  { get; set; }
+		public DateTime timp3  { get; set; }
+		public bool ttt  { get; set; }
+		public long rfsdf  { get; set; }
+		public decimal sdcsdfsdf  { get; set; }
+		public Guid dsdsdd  { get; set; }
 	}
 }
---
+++ WebPool/WebPool/DB/DBModels/UserOpenAnswer.generated.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.CodeDom.Compiler;
+ 
+namespace WebPool.DB.DBModels
+{
+    /// <summary>
+    /// UserOpenAnswer auto generated enumeration
+    /// </summary>
+    [GeneratedCode("TextTemplatingFileGenerator", "10")]
+    public class UserOpenAnswer
+    {
+		public int ID  { get; set; }
+		public string Answer  { get; set; }
+		public int QuestionID  { get; set; }
+		public short UserID  { get; set; }
+	}
+}
