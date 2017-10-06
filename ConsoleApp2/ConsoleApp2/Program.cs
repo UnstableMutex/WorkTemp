2b49cdde0d51165e35e0c4ad184734b635810855
Dmitry <stormbringer-s@mail.ru>
2017-10-06 16:17:37 +0400

    fgb

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,74 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Linq.Expressions;
 namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var c = new Calcer();
-            var stepsize = c.CalcStepSize();
-            var result = 0M;
-            var steps = 500;
-            for (int i = 0; i < steps; i++)
+            
+            Expression<Func<Entity, bool>> f = e => e.Test == "5";
+            var aa = f.Body as BinaryExpression;
+            var l = aa.Left as MemberExpression;
+            var method = aa.Method.Name;
+            var property = l.Member.Name;
+            var r = aa.Right as ConstantExpression;
+            var v = r.Value.ToString();
+            string Cond=string.Empty;
+            if (method == "op_Equality")
             {
-                result += (Calcer.StartPrice - (stepsize * i));
 
+                Cond = property + surr("=") + r;
             }
-            Console.WriteLine("result={0}",result);
-            Console.ReadKey();
+            string sel = string.Empty;
+
+            var t = typeof(Entity);
+         var props =  t.GetProperties().Select(x => x.Name);
+
+
+
+
+
+            Cond = "WHERE " + Cond;
         }
+
+        static string surr(string s)
+        {
+            return " " + s + " ";
+        }
+
     }
-    class Calcer
+
+    class Entity
     {
-
-        public decimal CalcStepSize()
-        {
-            for (decimal i = 1.8770M; i < 2.1M; i = i + .0000001M)
-            {
-                x = i;
-                var res = StepPrice();
-                Console.WriteLine(res);
-
-                if (res < TotalCost)
-                {
-                    Console.WriteLine("x={0}", i);
-                    Console.WriteLine("step={0}", StepSize());
-                    return StepSize();
-
-                }
-            }
-            throw new Exception();
-        }
-
-        public const int StepCount = 500;
-        public const decimal StartPrice = 5;
-        public const decimal EndPrice = 1.5M;
-        private const decimal TotalCost = 860;
-        const decimal Coridor = StartPrice - EndPrice;
-        private decimal startx = 0.01M;
-        private decimal x;
-        decimal StepPrice()
-        {
-            decimal res = 0;
-            for (int i = 0; i < 500; i++)
-            {
-                res += OneIter(i);
-            }
-            return res;
-        }
-        decimal OneIter(int step)
-        {
-            return StartPrice - step * StepSize();
-        }
-
-        decimal StepSize()
-        {
-            return ((Coridor * x) / StepCount);
-        }
+        public int ID { get; set; }
+        public string Test { get; set; }
     }
 }
